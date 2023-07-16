ff607a35817be7b4a0176bb785199ef1998c87fe
UrsulaScharler <139688545+UrsulaScharler@users.noreply.github.com>
2023-07-16 17:44:38 +0200

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/UrsulaScharler/UrsulaScharler.github.io";
+const repoURL = "https://github.com/UrsulaScharler/UrsulaScharler.github.io.git";
 
 ghpages.publish(
   pathname,
